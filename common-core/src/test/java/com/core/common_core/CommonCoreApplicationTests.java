2f747a7ba4a2408158549474725ed351d2cdc2de
onehowon <flash0221@naver.com>
2025-01-23 23:51:45 +0900

    add: 코어 프로젝트 구조 생성

---
+++ common-core/src/test/java/com/core/common_core/CommonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.common_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
